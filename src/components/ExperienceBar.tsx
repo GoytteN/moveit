1ce9644afe0b1afc378901da3ec36ef33d80d5bc
João Pedro <joaodev84@gmail.com>
2021-02-22 15:48:10 -0300

    Create the Experience Bar component & set up workspace

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0px</span>
+      <div>
+        <div style={{ width: '50%' }}/>
+
+        <span className="currenct-experience" style={{ left: '50%' }}>
+          300xp
+        </span>
+      </div>
+      <span>600px</span>
+    </header>
+  );
+}
