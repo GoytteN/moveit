5941f8d9694c34c8ddd3e8ad08c7238f85d38983
João Pedro <joaodev84@gmail.com>
2021-02-25 21:53:31 -0300

    Create the Challenge Box component  and button functions

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,32 +1,23 @@
-<<<<<<< HEAD
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/ExperienceBar.module.css";
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext)
+
+  const percentToNextLevel = Math.round((currentExperience * 100)) / experienceToNextLevel;
+
   return (
     <header className={ styles.experienceBar }>
       <span>0 xp</span>
       <div>
-        <div style={{ width: '50%' }}/>
+        <div style={{ width: `${percentToNextLevel}%` }}/>
 
-        <span className={ styles.currentExperience } style={{ left: '50%' }}>
-          300 xp
+        <span className={ styles.currentExperience } style={{ left: `${percentToNextLevel}%` }}>
+          {currentExperience} xp
         </span>
       </div>
-      <span>600 xp</span>
-=======
-export function ExperienceBar() {
-  return (
-    <header className="experience-bar">
-      <span>0px</span>
-      <div>
-        <div style={{ width: '50%' }}/>
-
-        <span className="currenct-experience" style={{ left: '50%' }}>
-          300xp
-        </span>
-      </div>
-      <span>600px</span>
->>>>>>> 1ce9644afe0b1afc378901da3ec36ef33d80d5bc
+      <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
